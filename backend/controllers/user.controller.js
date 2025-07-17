3aa7167b695049ffeb09aac8c472762ae9c898ae
hamza-amjad16 <freeroomcardw@gmail.com>
2025-07-17 14:42:46 +0500

    Cloudinary Setup for Resume link

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req, res) => {
   try {
@@ -125,6 +127,8 @@ export const updateProfile = async(req , res) => {
         const {fullname , email , phoneNumber , bio , skills} = req.body
         const file = req.file
     // cloudinary file here
+      const fileUri = getDataUri(file)
+      const cloudResponse = await cloudinary.uploader.upload(fileUri.content)
 
     let skillsArray;
     if(skills) skillsArray = skills.split(",")
@@ -147,6 +151,10 @@ export const updateProfile = async(req , res) => {
    if(skills) user.profile.skills = skillsArray
 
     // resume comes later Url of cloudinary
+    if(cloudResponse){
+      user.profile.resume = cloudResponse.secure_url  // save the cloudinary url
+      user.profile.resumeOriginalname = file.originalname 
+    }
 
     await user.save()
 
