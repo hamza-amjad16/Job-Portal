25e8ec85a9941f1bbd6b0774e56c9d81b83c66ad
hamza-amjad16 <freeroomcardw@gmail.com>
2025-08-01 23:16:45 +0500

    Deployment Minor changes in cookies

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -103,9 +103,10 @@ export const login = async (req, res) => {
     return res
       .status(200)
       .cookie("token", token, {
-        maxAge: 1 * 24 * 60 * 60 * 100,
-        httpsOnly: true,
-        sameSite: "strict",
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        httpOnly: true,
+        secure: true, 
+        sameSite: "None",
       })
       .json({
         message: "Login Successfully",
